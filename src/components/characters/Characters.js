aefa7f36b2dae99fa7107abf65041a8336a3b11f
Lena Hou <lenahou2013@gmail.com>
2022-01-05 18:53:17 -0500

    refactor other components using state and effect hooks

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,12 +1,35 @@
 import React, { } from 'react';
+import { useState, useEffect } from 'react/cjs/react.development';
 import { fetchSWAPI } from '../utils';
 
 
 function Characters(){
+    const [characters, setCharacters] = useState([])
+    
+    // componentDidMount <= make a fetch request in this lifecycle method
+    // we need an empty array for dependency as a second argument to get rid of looping 
+    useEffect(() => {
+        fetchSWAPI("people")
+          .then(data => setCharacters(data.results))
+        
+        // componentWillUnMount <= should return a callback function
+        // this is a CLEANUP function that will reset the planets back to default
+        return () => {
+              console.log("Characters component unmounted")
+              setCharacters([])
+        }      
+    }, [])
+
+    // componentDidUpdate
+    useEffect(() => {
+        console.log("characters updated")  
+    }, [characters])
+
     return (
         <div>
             <h1>All Characters</h1>
             <ul>
+                {characters.map((c, idx) => <li key={idx}>{c.name}</li>)}
             </ul>
         </div>
     );
--- src/components/planets/Planets.js
+++ src/components/planets/Planets.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchSWAPI } from '../utils';
 
 function Planets(){
+    // STEP 1: use state hook to define our state
+    const [planets, setPlanets] = useState([])
+    
+    // STEP 2: make a fecth request to fecth data
+    // componentDidMount
+    useEffect(() => {
+        fetchSWAPI("planets")
+          .then(data => setPlanets(data.results))
+          
+        // componentWillUnMount <= needs to return a callback function
+        // this is a CLEANUP function that will reset the planets back to default
+        return () => {
+            console.log("Planets going to unmount")
+            setPlanets([])
+        }  
+    }, [])
+
+
+    // STEP 4 - check if the data needs update
+    // componentDidUpdate
+    useEffect(() => {
+        console.log("Planets needs update!")
+    }, [planets])
+
+    // STEP 3- map the data to the DOM
     return (
         <div>
             <h1>All Planets</h1>
             <ul>
+                {planets.map((p, idx) => <li key={idx}>{p.name}</li>)}
             </ul>
         </div>
     );
